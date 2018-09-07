7e4dd9051dc1b669d0707e96dccc8dec633e84c8
Raghu K <raghuk.vit@gmail.com>
2018-09-07 01:32:43 +0000

    ss

--- pull.sh
+++ pull.sh
@@ -1,9 +1,8 @@
 repos=(http://root:password@104.196.127.109/engineers/ansible.git)
 for repo in ${repos[*]}; do 
-  DIR=$(echo $repo | awk -F / '{print $NF}'|sed -e '/.git//' )
+  DIR=$(echo $repo | awk -F / '{print $NF}'|sed -e 's/.git//' )
   rm -rf $DIR
   git clone $repo
   rm -rf $DIR/.git
 done
-
 git add -A ; git commit -m ss; git push origin master
